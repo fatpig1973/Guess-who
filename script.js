dcd38eba8855e307638b0199d3e7bbf021a9f7c9
Kevin <44airdiver@gmail.com>
2023-03-29 20:37:45 +0700

    Click event 2
    attempt to repair click event, still receiving error in dev tools

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 console.log('Guess Who?')
-// parameter named 'event' , it represents the event OBJECT that is passed to the funtionwhen the 'click' event is triggered
 
-function toggleEliminated(event) {
-    let cardSelected = event.target
-    cardSelected.classList.toggle('eliminated')
-  }
+    
+
 //   locates the first 'card' class that have the 'onclick' event
-  let cards = document.querySelectorAll('.card')
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].onclick = toggleEliminated
-  }
+function toggleEliminated() {
+    this.classList.toggle('eliminated')
+}
+let someCards = document.querySelectorAll('.card')
+for (let i = 0; i < someCards.length; i++) {
+    someCards[i].onclick = toggleEliminated
+}
+
 //   queryselectorall retuns a nodelist, the for loop iterates through the list and applies the function to each
+
+
