a24fc57b20541fbd8316a7182ac082e91b944eb5
Kevin <44airdiver@gmail.com>
2023-03-29 12:33:44 +0700

    Add a "click" event

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('Guess Who?')
+// parameter named 'event' , it represents the event OBJECT that is passed to the funtionwhen the 'click' event is triggered
+
+function toggleEliminated(event) {
+    let cardSelected = event.target
+    cardSelected.classList.toggle('eliminated')
+  }
+//   locates the first 'card' class that have the 'onclick' event
+  let cards = document.querySelectorAll('.card')
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].onclick = toggleEliminated
+  }
+//   queryselectorall retuns a nodelist, the for loop iterates through the list and applies the function to each
