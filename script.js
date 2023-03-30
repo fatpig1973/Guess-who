3844b8cff1f9b6c79a2931102bc22319eb78e9df
Kevin <44airdiver@gmail.com>
2023-03-30 11:24:26 +0700

    Click vent, no errors

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 console.log('Guess Who?')
 
-    
-
-//   locates the first 'card' class that have the 'onclick' event
-function toggleEliminated() {
-    this.classList.toggle('eliminated')
-}
-let someCards = document.querySelectorAll('.card')
-for (let i = 0; i < someCards.length; i++) {
-    someCards[i].onclick = toggleEliminated
-}
-
-//   queryselectorall retuns a nodelist, the for loop iterates through the list and applies the function to each
-
-
+function toggleEliminated(img) {
+    img.classList.toggle("eliminated");
+ }
+//  classList.toggle toggles the class 'eliminate' on the 'img' element
